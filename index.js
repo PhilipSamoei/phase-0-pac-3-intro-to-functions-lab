aa6d9561ff62495c5b71c496ffb46951f1139e34
Philip Samoei <samphilkerich04@gmail.com>
2023-03-11 13:08:55 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log('HELLO');
+}
+function logWhisper(string) {
+    console.log('hello');
+  }
+
+function sayHiToGrandma(string) {
+    if (string === 'toLowerCase') {
+      console.log("I can\'t hear you!");
+      } else {
+      console.log("hello");
+      }
+
+      if (string === 'toUpperCase') {
+        console.log("YES INDEED!");
+        } else {
+        console.log("HELLO");
+        }
+
+        if (string === 'Let\'s have dinner together!') {
+          console.log("I would love to!");
+          } else {
+          console.log("Let's have dinner together!");
+          }
+
+    }
+
